2aec48f033656c20faa27af8a224f5351dd414a6
José Cuevas <josecueram@gmail.com>
2019-03-18 20:03:06 -0600

    Added basic routes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,19 @@ const chalk = require('chalk')
 const app = express()
 
 app.get('/', (request, response) => {
-    response.send('Hello express')
+    response.send('Hello express!')
+})
+
+app.get('/help', (request, response) => {
+    response.send('Help page.')
+})
+
+app.get('/about', (request, response) => {
+    response.send('About page.')
+})
+
+app.get('/weather', (request, response) => {
+    response.send('Weather page.')
 })
 
 app.listen(3000, () => {
