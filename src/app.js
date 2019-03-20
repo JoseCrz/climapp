c414f7e2eae1062e920b42860972fdc3cce982e8
José Cuevas <josecueram@gmail.com>
2019-03-20 17:57:23 -0600

    Added partials

--- src/app.js
+++ src/app.js
@@ -4,17 +4,20 @@ const path = require('path')
 //NPM modules
 const express = require('express')
 const chalk = require('chalk')
+const hbs = require('hbs')
 
 
 const app = express()
 //*------------------CONFIGURATION------------------*
 //Paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 
 //Handlebars config
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -25,7 +28,8 @@ app.use(express.static(publicDirectoryPath))
 //Routes
 app.get('/', (request, response) => {
     response.render('index', {
-        title: 'Weather App'
+        title: 'Weather App',
+        name: 'José Cuevas'
     })
 })
 
@@ -39,7 +43,8 @@ app.get('/about', (request, response) => {
 app.get('/help', (request, response) => {
     response.render('help', {
         title: 'Help',
-        message: 'This is a help message'
+        message: 'This is a help message',
+        name: 'José Cuevas'
     })
 })
 
