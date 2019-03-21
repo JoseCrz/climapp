62d216c381cf50480eca05c6123759aa1bb09303
José Cuevas <josecueram@gmail.com>
2019-03-20 22:24:20 -0600

    Added logic to the search form

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,24 @@
-console.log('Hello client side JavaScript')
+const weatherForm = document.querySelector('form')
+const textInput = document.querySelector('input')
 
-fetch('http://localhost:3000/weather?location=manchester')
-.then(response => {
+weatherForm.addEventListener('submit', event => {
+    event.preventDefault()
     
-    response.json()
-    .then(data => {
-        if (data.error) {
-            console.log(data.error)
+    const location = textInput.value
+    
 
-        } else {
-            console.log(data)
-        }
+    fetch(`http://localhost:3000/weather?location=${location}`)
+    .then(response => {
+        
+        response.json()
+        .then(data => {
+            if (data.error) {
+                console.log(data.error)
+    
+            } else {
+                console.log(data)
+            }
+        })
     })
 })
+
