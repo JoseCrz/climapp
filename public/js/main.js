19282d2f727378a1efe0cdc7b23b9c6ba30c4008
José Cuevas <josecueram@gmail.com>
2019-03-20 22:50:12 -0600

    Added logic to display weather information to the user

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const weatherForm = document.querySelector('form')
 const textInput = document.querySelector('input')
+const successMessage = document.getElementById('success-message')
+const errorMessage = document.getElementById('error-message')
 
 weatherForm.addEventListener('submit', event => {
     event.preventDefault()
@@ -14,9 +16,13 @@ weatherForm.addEventListener('submit', event => {
         .then(data => {
             if (data.error) {
                 console.log(data.error)
+                errorMessage.textContent = data.error
+                successMessage.textContent = ''
     
             } else {
                 console.log(data)
+                errorMessage.textContent = ''
+                successMessage.textContent = `${data.location}. ${data.summary} current temperature ${data.temperature}, with a ${data.rainChance*100}% chance of raining`
             }
         })
     })
